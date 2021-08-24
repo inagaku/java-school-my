067206da4d86bdfe03982a1ed86e7410906e60f5
nagaku <nikita.v.agafonov@gmail.com>
2021-08-24 11:53:20 +0300

    Yesterday

--- src/main/java/homework/java8/streams/yesterdey/WordCounter.java
+++ src/main/java/homework/java8/streams/yesterdey/WordCounter.java
@@ -10,7 +10,9 @@ import java.util.Arrays;
 public class WordCounter {
     @SneakyThrows
     public static long countWords(String song) {
-        return Files.lines(Path.of(song)).flatMap(s -> Arrays.stream(s.split("\\s+"))).count();
+        return Files.lines(Path.of(song))
+                .flatMap(s -> Arrays.stream(s.split("\\s+")))
+                .count();
     }
 
     public static void main(String[] args) {
